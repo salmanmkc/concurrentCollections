83852b4ea34f6a67dac7e6ae36a5d1e55659a765
SalmanMKC <32169182+salmanmkc@users.noreply.github.com>
2019-02-11 12:27:44 +0000

    done with tasks

---
+++ 1/1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task1 = new Task(() => orderProducts("mark"));
+            Task task2 = new Task(() => orderProducts("Lucy"));
+            task1.Start();
+            task2.Start();
+            Task.WaitAll(task1, task2);
+        }
+
+
+        static void orderProducts(string name)
+        {
+            for(int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{name} order number {i}.");
+            }
+        }
+    }
+
+
+}
